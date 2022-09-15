fd366c9524d3d894a888bc605d96a1af70eda501
rahmanabdulquadir <rahmanaq777@gmail.comm>
2022-09-15 13:43:38 +0600

    countdown clock seconds converted to integer

--- script.js
+++ script.js
@@ -113,7 +113,7 @@ const start = () => {
     if (count === 0) {
       // -------------- START TYPING -----------------
       document.addEventListener("keydown", typeController);
-      // countdownOverlay.style.display = "flex";
+      countdownOverlay.style.display = "flex";
       countdownOverlay.style.display = "none";
       display.classList.remove("inactive");
 
@@ -133,7 +133,7 @@ displayHistory();
 // Show typing time spent
 setInterval(() => {
   const currentTime = new Date().getTime();
-  const timeSpent = (currentTime - startTime) / 1000;
+  const timeSpent = Math.round((currentTime - startTime) / 1000);
 
 
   document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0} seconds`;
